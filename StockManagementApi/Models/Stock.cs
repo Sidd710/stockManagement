85e7d79940683fb6a4f07441fe7a11ecbb7148e5
Siddharth Shah <sidd710@gmail.com>
2020-02-14 16:25:22 +0530

    change in parameter name

--- StockManagementApi/Models/Stock.cs
+++ StockManagementApi/Models/Stock.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,33 +9,55 @@ namespace StockManagementApi.Models
     public class Stock
     {
         // public int Id { get; set; }
+        [JsonProperty("Sid")]
         public int StockInId { get; set; }
+        [JsonProperty("Lid")]
         public string LotBatchId { get; set; }
+        [JsonProperty("Pid")]
         public int ProductId { get; set; }
+        [JsonProperty("Dor")]
         public DateTime DateOfReceipt { get; set; }
+        [JsonProperty("Crv")]
         public string CrvNumber { get; set; }
+        [JsonProperty("Des")]
         public string Description { get; set; }
+        [JsonProperty("Rcf")]
         public string ReceivedFrom { get; set; }
+        [JsonProperty("Pmn")]
         public string PackingMaterialName { get; set; }
+        [JsonProperty("Omn")]
         public string OriginalManufacture { get; set; }
+        [JsonProperty("Gn")]
         public string GenericName { get; set; }
+        [JsonProperty("We")]
         public decimal Weight { get; set; }
-        //public int AddedBy { get; set; }
-        //public DateTime AddedOn { get; set; }
+        [JsonProperty("Aby")]
+        public int AddedBy { get; set; }
+        [JsonProperty("Abo")]
+        public DateTime AddedOn { get; set; }
         //public int ModifiedBy { get; set; }
         //public DateTime ModifiedOn { get; set; }
+        [JsonProperty("Is")]
         public Boolean IsActive { get; set; }
 
     }
     public class Batch
     {
+        [JsonProperty("Bid")]
         public int BatchId { get; set; }
+        [JsonProperty("Bn")]
         public string BatchName { get; set; }
+        [JsonProperty("Qu")]
         public int Quantity { get; set; }
+        [JsonProperty("Wsi")]
         public int WarehouseSectionId { get; set; }
+        [JsonProperty("Mfg")]
         public DateTime MfgDate { get; set; }
+        [JsonProperty("Exp")]
         public DateTime ExpDate { get; set; }
+        [JsonProperty("Esl")]
         public DateTime EslDate { get; set; }
+        [JsonProperty("Avl")]
         public int AvailableQuantity { get; set; }
 
 
@@ -42,19 +65,27 @@ namespace StockManagementApi.Models
 
     public class ProductQuantity
     {
+        [JsonProperty("Id")]
         public int Id { get; set; }
+        [JsonProperty("Pid")]
         public int ProductId { get; set; }
+        [JsonProperty("Qua")]
         public int Quantity { get; set; }
     }
     public class StockIn
     {
+        [JsonProperty("Bt")]
         public List<Batch> Batch { get; set; }
+        [JsonProperty("St")]
         public Stock stock { get; set; }
     }
     public class StockOut
     {
+        [JsonProperty("Lid")]
         public int LotBatchId { get; set; }
+        [JsonProperty("Qua")]
         public int Quantity { get; set; }
+        [JsonProperty("Des")]
         public DateTime DateOfDispatch { get; set; }
     }
     public class ViewStockIn
