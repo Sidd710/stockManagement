918ebe98dc058841866540145cac50dad4791f1a
kaushal.soni <kaushal.soni@radixweb.com>
2020-05-15 23:51:17 +0530

    made changes for IDTICT in controller file

---
+++ StockManagementApi/Controllers/IDTICTController.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using StockManagementApi.Models;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StockManagementApi.Controllers
+{
+    public class IDTICTController : ApiController
+    {
+        string sqlConnectionString = ConfigurationManager.AppSettings["Default"];
+        [HttpPost]
+        public async Task<IHttpActionResult> AddIdtIctIn([FromBody]IdtIctInModel value)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ StockManagementApi/Models/IdtIctInModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockManagementApi.Models
+{
+    public class IdtIctInModel
+    {
+        public firstForm firstForm { get; set; }
+        public List<depotValueModel> depotValue { get; set; }
+        public List<depotProductValueModel> depotProdcutValueList { get; set; }
+    }
+
+    public class firstForm
+    {
+        public DateTime DateOfEntry { get; set; }
+        public string IdtIctType { get; set; }
+        public string ReferenceNumber { get; set; }
+    }
+    public class depotValue
+    {
+        public List<depotValueModel> depotValueList { get; set; }
+    }
+    public class depotValueModel
+    {
+        public int id { get; set; }
+        public string value { get; set; }
+        public bool Checked { get; set; }
+        public string date { get; set; }
+
+    }
+    public class depotProdcutValueList
+    {
+        public List<depotProductValueModel> depotProductList { get; set; }
+    }
+    public class depotProductValueModel
+    {
+        public int productId { get; set; }
+        public int depotId { get; set; }
+        public string quantity { get; set; }
+    }
+}
