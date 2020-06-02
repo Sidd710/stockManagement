05dd28d148ff52f13fb7b8a0248875060c4d29e1
Siddharth Shah <sidd710@gmail.com>
2020-06-02 22:57:11 +0530

    Edit Model

--- StockManagementApi/Models/IdtIctInModel.cs
+++ StockManagementApi/Models/IdtIctInModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace StockManagementApi.Models
         public firstForm firstForm { get; set; }
         public List<depotValueModel> depotValue { get; set; }
         public List<depotProductValueModel> depotProdcutValueList { get; set; }
+        public string status { get; set; }
     }
 
     public class firstForm
@@ -18,6 +20,7 @@ namespace StockManagementApi.Models
         public DateTime DateOfEntry { get; set; }
         public string IdtIctType { get; set; }
         public string ReferenceNumber { get; set; }
+        public string Status { get; set; }
     }
     public class depotValue
     {
@@ -37,15 +40,28 @@ namespace StockManagementApi.Models
     }
     public class depotProductValueModel
     {
+        [JsonProperty("pid")]
         public int productId { get; set; }
+        [JsonProperty("did")]
         public int depotId { get; set; }
         public string quantity { get; set; }
         public string date { get; set; }
         public int IdtIctMasterId { get; set; }
+        public int Id { get; set; }
     }
     public class IdtDetails
     {
         public dynamic IdtIcTMaster { get; set; }
         public dynamic IdtIctDetails { get; set; }
     }
+
+    public class EditIdtIctInModel
+    {
+        public int Id { get; set; }
+        public firstForm firstForm { get; set; }
+        public List<depotValueModel> depotValue { get; set; }
+        public List<depotProductValueModel> depotProdcutValueList { get; set; }
+        public string status { get; set; }
+    }
+
 }
