80566e978eeb1c092bd8a0ecf91b35c3416874b3
kaushal.soni <kaushal.soni@radixweb.com>
2020-07-08 22:09:10 +0530

    Update Depot changes

--- StockManagementApi/Controllers/DepotController.cs
+++ StockManagementApi/Controllers/DepotController.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Web.Http;
 using static StockManagementApi.Controllers.CategoryController;
 
@@ -48,9 +49,9 @@ namespace StockManagementApi.Controllers
                         Depot_Code = value.Depot_Code,
                         UnitName = value.UnitName,
                         IDT = value.IDT_bool == true ? "IDT" : null,
-                        ICT = value.ICT_bool == true ? "ICT" : null,    
+                        ICT = value.ICT_bool == true ? "ICT" : null,
                         AWS = value.AWS_bool == true ? "AWS" : null,
-                        IsMother=value.IsMother
+                        IsMother = value.IsMother
                     };
                     p.Depu_Id = connection.Query<int>(@"insert depumaster(Depu_Name,Corp,FormationId,IsActive,Addedon,ModifiedOn,IsParent,Depu_Location,Depot_Code,UnitName,IDT,ICT,AWS,IsMother) values (@Depu_Name,@Corp,@FormationId,@IsActive,@Addedon,@ModifiedOn,@IsParent,@Depu_Location,@Depot_Code,@UnitName,@IDT,@ICT,@AWS,@IsMother) select cast(scope_identity() as int)", p).First();
 
@@ -81,7 +82,7 @@ namespace StockManagementApi.Controllers
             var list = DataTableToJSONWithJSONNet(dt);
             dynamic json = JsonConvert.DeserializeObject(list);
 
-            depotListData.DepotList=json;
+            depotListData.DepotList = json;
             return depotListData;
 
         }
@@ -122,6 +123,100 @@ namespace StockManagementApi.Controllers
             string JSONString = string.Empty;
             JSONString = JsonConvert.SerializeObject(table);
             return JSONString;
+        }
+
+        public dynamic GetByDepotId(int Id)
+        {
+            var depot = new DepotList();
+            var connection = new SqlConnection(sqlConnectionString);
+            depot = connection.Query<DepotList>("Select * from DepuMaster where Depu_Id = @Id", new { Id = Id }).FirstOrDefault();
+            if (depot.AWS != null && depot.AWS == "AWS")
+            {
+                depot.AWS_bool = true;
+            }
+            else if (depot.ICT != null && depot.ICT == "ICT")
+            {
+                depot.ICT_bool = true;
+            }
+            else if (depot.IDT != null && depot.IDT == "IDT")
+            {
+                depot.IDT_bool = true;
+            }
+
+            return depot;
+        }
+
+        [HttpPost]
+
+        public async Task<IHttpActionResult> UpdateDepot([FromBody]DepotList value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (TransactionScope scope = new TransactionScope())
+            using (var connection = new SqlConnection(sqlConnectionString))
+            {
+
+
+                connection.Open();
+                var AWS = string.Empty;
+                var ICT = string.Empty;
+                var IDT = string.Empty;
+                if (value.AWS_bool)
+                {
+                    AWS = "AWS";
+                }
+                else if (value.ICT_bool)
+                {
+                    ICT = "ICT";
+                }
+                else if (value.IDT_bool)
+                {
+                    IDT = "IDT";
+                }
+                var depot = new DepotList()
+                {
+                    Depu_Name = value.Depu_Name,
+                    Depu_Location = value.Depu_Location,
+                    AWS = AWS,
+                    Corp = value.Corp,
+                    FormationId = value.FormationId,
+                    ICT = ICT,
+                    IDT = IDT,
+                    IsActive = value.IsActive,
+                    IsMother = value.IsMother,
+                    IsParent = value.IsParent,
+                    ModifiedBy = 0,
+                    ModifiedOn = DateTime.Now,
+                    UnitName = value.UnitName,
+                    Depu_Id = value.Depu_Id
+                };
+
+
+
+
+                string updateQuery = @"Update DepuMaster 
+                                              Set 
+                                                Depu_Name = @Depu_Name ,
+                                                Depu_Location = @Depu_Location,
+                                                AWS=@AWS,
+                                                Corp=@Corp ,
+                                                FormationId = @FormationId,
+                                                ICT=@ICT,
+                                                IDT = @IDT,
+                                                IsActive=@IsActive,
+                                                IsMother=@IsMother,
+                                                IsParent=@IsParent,
+                                                ModifiedBy=@ModifiedBy,
+                                                ModifiedOn=@ModifiedOn,
+                                                UnitName=@UnitName
+                                              Where Depu_Id = @Depu_Id";
+
+                var result = connection.Execute(updateQuery, depot);
+                scope.Complete();
+                return Json(new { Message = "Record Updated Successfully" });
+            }
         }
     }
 }
