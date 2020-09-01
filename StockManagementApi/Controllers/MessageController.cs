3b9555c79a1dec9b70dacde078259ec5e5cd9a63
Siddharth Shah <sidd710@gmail.com>
2020-09-01 13:03:57 +0530

    Message Test API

---
+++ StockManagementApi/Controllers/MessageController.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using StockManagementApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StockManagementApi.Controllers
+{
+    public class MessageController : ApiController
+
+    {
+        string sqlConnectionString = ConfigurationManager.AppSettings["Default"];
+        [HttpPost]
+        public async Task<IHttpActionResult> AddMesage([FromBody]MessageName message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            using (var connection = new SqlConnection(sqlConnectionString))
+            {
+                connection.Open();
+
+                var p = new MessageName { Message = message.Message};
+                p.MessageId = connection.Query<int>(@"insert tbl_message(Message) values (@Message) select cast(scope_identity() as int)", p).First();
+
+                return Json(new { Message = "Record Inserted Successfully" });
+            }
+
+
+        }
+    }
+}
---
+++ StockManagementApi/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockManagementApi.Models
+{
+    public class MessageName
+    {
+
+        public int MessageId { get; set; }
+        public string Message { get; set; }
+    }
+}
